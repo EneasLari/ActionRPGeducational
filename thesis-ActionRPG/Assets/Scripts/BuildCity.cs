fd84945a294f51503ce4118d67d2612625c1bd1f
EneasLari <cs02367@cc.uoi.gr>
2018-02-25 00:52:42 +0200

    building and finishing world generation
    The streets can be loaded from txt file where each symbols express a street tile..The buildings are being generated with math function (noise)

--- thesis-ActionRPG/Assets/Scripts/BuildCity.cs
+++ thesis-ActionRPG/Assets/Scripts/BuildCity.cs
@@ -2,187 +2,207 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System;
 
-public class BuildCity : MonoBehaviour {
+public class BuildCity : MonoBehaviour
+{
 
-    //public GameObject[] buldings;
-    private int[,] mapGrid;
-    public GameObject corneRoadRtop;//1
-    public GameObject corneRoadLtop;//2
-    public GameObject corneRoadRdn;//3
-    public GameObject corneRoadLdn;//4
-    public GameObject xRoad;//5
-    public GameObject zRoad;//6
-    public GameObject crossRoad;//7
-    public GameObject tafRRoad;//8
-    public GameObject tafLRoad;//9
-    public GameObject tafUpRoad;//10
-    public GameObject tafDownRoad;//11
+    public GameObject[] buldings;
+    private char[,] mapGrid;
+
+    public GameObject corneRoadRtop; public char corneRoadRtopID = '<';
+    public GameObject corneRoadLtop; public char corneRoadLtopID = '>';
+    public GameObject corneRoadRdn; public char corneRoadRdnID= ']';
+    public GameObject corneRoadLdn; public char corneRoadLdnID = '[';
+    public GameObject crossRoad; public char crossRoadID = '+';
+    public GameObject tafRRoad; public char tafRRoadID = '{';
+    public GameObject tafLRoad; public char tafLRoadID = '}';
+    public GameObject tafUpRoad; public char tafUpRoadID = '^';
+    public GameObject tafDownRoad; public char tafDownRoadID = '_';
+    public GameObject xRoad; public char xRoadID = '=';
+    public GameObject zRoad; public char zRoadID = '|';
+
+    public string mapTxtFileName = "fileEneas.txt";
+    public int seed = 100;//UnityEngine.Random.Range(0,100);
     private int numofRoads = 7;
     public int mapWidth = 30;
     public int mapHeight = 30;
-    private int area;
-    public float RoadsDensity = 10;
-    [Range(0, 100)]
-    private int envObjectFootprint = 2;
-
+    private int envObjectFootprint = 3;
 
     // Use this for initialization
-    void Start() {
-        mapGrid = new int[mapWidth,mapHeight];
-        for (int h = 0; h < mapHeight; h++) {
-            for (int w = 0; w < mapWidth; w++) {
-                //Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
-                //int n = Random.Range(0, buldings.Length);
-                //Instantiate(buldings[n], pos, Quaternion.identity);
-            }
-        }
-
-        LoadRoadsFromText("fileEneas.txt");
-        InstantiateRoads();
-    }
-
-
-    private void InstantiateRoads() { 
-            for (int h = 0; h < mapHeight; h++)
-            {
-                for (int w = 0; w < mapWidth; w++)
-                {
-                    if (mapGrid[h, w] == 1)
-                    {
-                        Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
-                        Instantiate(corneRoadRtop, pos, corneRoadRtop.transform.rotation);
-                    }
-                    if (mapGrid[h, w] == 2)
-                    {
-                        Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
-                        Instantiate(corneRoadLtop, pos, corneRoadLtop.transform.rotation);
-                    }
-                    if (mapGrid[h, w] == 3)
-                    {
-                        Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
-                        Instantiate(corneRoadRdn, pos, corneRoadRdn.transform.rotation);
-                    }
-                    if (mapGrid[h, w] == 4)
-                    {
-                        Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
-                        Instantiate(corneRoadLdn, pos, corneRoadLdn.transform.rotation);
-                    }
-                    if (mapGrid[h,w]==5) {
-                        Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
-                        Instantiate(xRoad, pos,xRoad.transform.rotation);
-                    }
-                    if (mapGrid[h, w] == 6)
-                    {
-                        Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
-                        Instantiate(zRoad, pos, zRoad.transform.rotation);
-                    }
-
-                    if (mapGrid[h, w] == 7)
-                    {
-                        Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
-                        Instantiate(crossRoad, pos, corneRoadRtop.transform.rotation);
-                    }
-                    if (mapGrid[h, w] == 8)
-                    {
-                        Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
-                        Instantiate(tafRRoad, pos, tafRRoad.transform.rotation);
-                    }
-                    if (mapGrid[h, w] == 9)
-                    {
-                        Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
-                        Instantiate(tafLRoad, pos, tafLRoad.transform.rotation);
-                    }
-                }
-            }  
-    }
-
-    private bool LoadRoadsFromText(string fileName)
+    void Start()
     {
-        StreamReader inpStream = new StreamReader(fileName);
-        int h = 0; int w=0;
-        while (!inpStream.EndOfStream && h < mapHeight)
+        mapGrid = new char[mapWidth, mapHeight];
+        readDataFromFile(mapTxtFileName);
+        InstantiateRoads();
+        RandomBuildings();
+    }
+
+
+    private void RandomBuildings()
+    {
+        for (int h = 0; h < mapHeight; h++)
         {
-            string line = inpStream.ReadLine();
-            string[] entries = line.Split(' ');
-            while (w<mapWidth && w<entries.Length) {
-                print(entries[w]);
-                mapGrid[mapHeight-h-1, w] = System.Int32.Parse(entries[w]);
-                w++;
-            }
-            w = 0;
-            h++;
-
-             
-        }
-
-        inpStream.Close();
-        return true;
-    }
-
-
-    private void BuildGrid() {
-        int x = Random.Range(0, mapWidth);
-        int z = Random.Range(0, mapHeight);
-        int roadintersection = 8;// Random.Range(8,12);
-        int cornerRoad = Random.Range(1, 5);
-
-        mapGrid[z, x] = roadintersection;
-        if (roadintersection==8) {//An einai T pou koitaei deksia,proekteine ta akra
-            int tempx = x+1;
-            int tempz = z;
-            while (tempx < mapWidth) {
-
-                if (mapGrid[tempz, tempx] == 6)
-                {
-                    mapGrid[tempz, tempx] = 7;
-                }
-                else if (mapGrid[tempz, tempx] == 5)
-                {
-                    mapGrid[tempz, tempx] = 7;
-                }
-                else {
-                    mapGrid[tempz, tempx] = 5;
-                }
-                
-                tempx++;
-            }
-            tempx = x;
-            tempz++;
-            while (tempz < mapHeight)
+            for (int w = 0; w < mapWidth; w++)
             {
-                if (mapGrid[tempz, tempx] == 5)
-                {
-                    mapGrid[tempz, tempx] = 7;
+                if (mapGrid[h, w] == '#') {
+                    int result = (int)(Mathf.PerlinNoise(w / 10f + seed, h / 10f + seed) * 18);
+                    Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
+                    if (result < 3)
+                    {
+                        Instantiate(buldings[0], pos, Quaternion.identity);
+                    }
+                    else if (result < 4)
+                    {
+                        Instantiate(buldings[1], pos, Quaternion.identity);
+                    }
+                    else if (result < 6)
+                    {
+                        Instantiate(buldings[2], pos, Quaternion.identity);
+                    }
+                    else if (result < 8)
+                    {
+                        Instantiate(buldings[3], pos, Quaternion.identity);
+                    }
+                    else if (result < 10)
+                    {
+                        Instantiate(buldings[4], pos, Quaternion.identity);
+                    }
+                    else if (result < 12)//pezodromos
+                    {
+                        GameObject inst= Instantiate(buldings[5], pos, Quaternion.identity);
+                        inst.transform.localScale=new Vector3(envObjectFootprint+.15f,inst.transform.localScale.y, envObjectFootprint+.15f);
+                    }
+                    else if (result < 18)
+                    {
+                        Vector3 pos2 = new Vector3(w * envObjectFootprint, 0.28f, h * envObjectFootprint);//for the trees only
+                        GameObject inst = Instantiate(buldings[6], pos2, Quaternion.identity);
+                        inst.transform.localScale = new Vector3(envObjectFootprint, inst.transform.localScale.y, envObjectFootprint);
+                    }
                 }
-                else if (mapGrid[tempz, tempx] == 6)
-                {
-                    mapGrid[tempz, tempx] = 7;
-                }
-                else
-                {
-                    mapGrid[tempz, tempx] = 6;
-                }
-                tempz++;
-            }
-            tempz = z-1;
-            while (tempz > 0)
-            {
-                if (mapGrid[tempz, tempx] == 5)
-                {
-                    mapGrid[tempz, tempx] = 7;
-                }
-                else if (mapGrid[tempz, tempx] == 6)
-                {
-                    mapGrid[tempz, tempx] = 7;
-                }
-                else
-                {
-                    mapGrid[tempz, tempx] = 6;
-                }
-                tempz--;
             }
         }
-
     }
+    private void InstantiateRoads()
+    {
+        for (int h = 0; h < mapHeight; h++)
+        {
+            for (int w = 0; w < mapWidth; w++)
+            {
+                if (mapGrid[h, w] == corneRoadRtopID)
+                {
+                    //croner road from down to right
+                    Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
+                    GameObject inst = Instantiate(corneRoadRtop, pos, corneRoadRtop.transform.rotation);
+                    inst.transform.localScale = new Vector3(inst.transform.localScale.x * envObjectFootprint, inst.transform.localScale.y, inst.transform.localScale.z * envObjectFootprint);
+                }
+                if (mapGrid[h, w] == corneRoadLtopID)
+                {
+                    //corner road from down to left
+                    Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
+                    GameObject inst = Instantiate(corneRoadLtop, pos, corneRoadLtop.transform.rotation);
+                    inst.transform.localScale = new Vector3(inst.transform.localScale.x * envObjectFootprint, inst.transform.localScale.y, inst.transform.localScale.z * envObjectFootprint);
+                }
+                if (mapGrid[h, w] == corneRoadRdnID)
+                {
+                    //corner road from up to left
+                    Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
+                    GameObject inst = Instantiate(corneRoadRdn, pos, corneRoadRdn.transform.rotation);
+                    inst.transform.localScale = new Vector3(inst.transform.localScale.x * envObjectFootprint, inst.transform.localScale.y, inst.transform.localScale.z * envObjectFootprint);
+                }
+                if (mapGrid[h, w] == corneRoadLdnID)
+                {
+                    //corner road from up to right
+                    Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
+                    GameObject inst = Instantiate(corneRoadLdn, pos, corneRoadLdn.transform.rotation);
+                    inst.transform.localScale = new Vector3(inst.transform.localScale.x*envObjectFootprint, inst.transform.localScale.y, inst.transform.localScale.z * envObjectFootprint);
+                }
+                if (mapGrid[h, w] == crossRoadID)
+                {
+                    //crossroad
+                    Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
+                    GameObject inst = Instantiate(crossRoad, pos, corneRoadRtop.transform.rotation);
+                    inst.transform.localScale = new Vector3(inst.transform.localScale.x * envObjectFootprint, inst.transform.localScale.y, inst.transform.localScale.z * envObjectFootprint);
+                }
+                if (mapGrid[h, w] == tafRRoadID)
+                {
+                    //taf road to right
+                    Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
+                    GameObject inst = Instantiate(tafRRoad, pos, tafRRoad.transform.rotation);
+                    inst.transform.localScale = new Vector3(inst.transform.localScale.x * envObjectFootprint, inst.transform.localScale.y, inst.transform.localScale.z * envObjectFootprint);
+                }
+                if (mapGrid[h, w] == tafLRoadID)
+                {
+                    //taf road to left
+                    Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
+                    GameObject inst = Instantiate(tafLRoad, pos, tafLRoad.transform.rotation);
+                    inst.transform.localScale = new Vector3(inst.transform.localScale.x * envObjectFootprint, inst.transform.localScale.y, inst.transform.localScale.z * envObjectFootprint);
+                }
+                if (mapGrid[h, w] == tafUpRoadID)
+                {
+                    //taf road to up z axis
+                    Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
+                    GameObject inst = Instantiate(tafUpRoad, pos, tafUpRoad.transform.rotation);
+                    inst.transform.localScale = new Vector3(inst.transform.localScale.x * envObjectFootprint, inst.transform.localScale.y, inst.transform.localScale.z * envObjectFootprint);
+                }
+                if (mapGrid[h, w] == tafDownRoadID)
+                {
+                    //taf road to up z axis
+                    Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
+                    GameObject inst = Instantiate(tafDownRoad, pos, tafDownRoad.transform.rotation);
+                    inst.transform.localScale = new Vector3(inst.transform.localScale.x * envObjectFootprint, inst.transform.localScale.y, inst.transform.localScale.z * envObjectFootprint);
+                }
+
+                if (mapGrid[h, w] == xRoadID)
+                {
+                    //road parallel to x axis
+                    Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
+                    GameObject inst = Instantiate(xRoad, pos, xRoad.transform.rotation);
+                    inst.transform.localScale = new Vector3(inst.transform.localScale.x * envObjectFootprint, inst.transform.localScale.y, inst.transform.localScale.z * envObjectFootprint);
+                }
+                if (mapGrid[h, w] == zRoadID)
+                {
+                    //road parallel to z axis
+                    Vector3 pos = new Vector3(w * envObjectFootprint, 0.5f, h * envObjectFootprint);
+                    GameObject inst = Instantiate(zRoad, pos, zRoad.transform.rotation);
+                    inst.transform.localScale = new Vector3(inst.transform.localScale.x * envObjectFootprint, inst.transform.localScale.y, inst.transform.localScale.z * envObjectFootprint);
+                }
+
+               
+
+            }
+        }
+    }
+
+    private void readDataFromFile(string fileName) {
+        try
+        {
+            using (StreamReader streamReader = new StreamReader(fileName))
+            {
+                int h = 0; int w = 0;
+                while (!streamReader.EndOfStream && h<mapHeight)
+                {
+                    string line = streamReader.ReadLine();
+                    foreach (char ch in line) {
+                        if (ch != ' ' && ch!='\t') {
+                            if (w < mapWidth) {
+                                mapGrid[mapHeight-h-1, w] = ch;
+                                w++;
+                            }          
+                        }
+                    }
+                    w = 0;
+                    h++;
+                }
+                streamReader.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log("The process failed: {0}"+ e.ToString());
+        }
+       
+    }
+
+    
 }
---
+++ thesis-ActionRPG/Assets/Scripts/RandomDesable_Enable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDesable_Enable : MonoBehaviour {
+    [Range(0,100)]
+    public float enablePropability = 20;
+	// Use this for initialization
+	void Start () {
+
+        if (enablePropability < Random.Range(0f, 100f)) {
+            gameObject.SetActive(false);
+        }
+	}
+	
+}
