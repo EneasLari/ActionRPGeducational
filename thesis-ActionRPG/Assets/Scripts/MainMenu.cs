b3ff19bc478ee22bb4531511b6e16d9a5177db2f
EneasLari <cs02367@cc.uoi.gr>
2019-02-14 00:34:11 +0200

    finishing main menu without functioning audio editing

--- thesis-ActionRPG/Assets/Scripts/MainMenu.cs
+++ thesis-ActionRPG/Assets/Scripts/MainMenu.cs
@@ -9,4 +9,9 @@ public class MainMenu : MonoBehaviour {
     public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
+
+    public void ExitGame() {
+        print("EXIT GAME");
+        Application.Quit();
+    }
 }
