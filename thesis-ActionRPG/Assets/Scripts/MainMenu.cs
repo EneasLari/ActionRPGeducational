06319b9a69fdea6f62b70b47c53b9e05c20350c3
EneasLari <cs02367@cc.uoi.gr>
2019-02-23 15:05:27 +0200

    fixing scaling issues at menus and changing some animations and npcs

--- thesis-ActionRPG/Assets/Scripts/MainMenu.cs
+++ thesis-ActionRPG/Assets/Scripts/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 
+    public void AtMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
     public void ExitGame() {
         print("EXIT GAME");
         Application.Quit();
